d9c923a0cfe9f20431870d96bcf7fa04449ef221
juzhang.fan <juzhang.fan@iraychina.com>
2017-11-30 16:46:17 +0800

    adjust the folder structure

--- cpp/rsa/genkey.sh
+++ cpp/rsa/genkey.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-openssl genrsa -out arm-pri.ky 1024
-openssl rsa -in arm-pri.ky -out arm-pub.ky -pubout
+#openssl genrsa -out arm-pri.ky 1024
+#openssl rsa -in arm-pri.ky -out arm-pub.ky -pubout
+openssl rsa -in arm.ky -outform der -out arm.der
+openssl rsa -in arm.ky -outform der -out armpub.der -pubout 
