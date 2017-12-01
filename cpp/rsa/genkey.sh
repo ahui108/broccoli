8c3447aa50f6d1c379a08411b41626ff97c31448
juzhang.fan <juzhang.fan@iraychina.com>
2017-12-01 09:27:36 +0800

    remove some none source code from source folder

--- cpp/rsa/genkey.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#openssl genrsa -out arm-pri.ky 1024
-#openssl rsa -in arm-pri.ky -out arm-pub.ky -pubout
-openssl rsa -in arm.ky -outform der -out arm.der
-openssl rsa -in arm.ky -outform der -out armpub.der -pubout 
